6387ad2fa046c1b0ac4918d6f33eb6161d7b819e
woshishupi <woshishupi@gmail.com>
2025-10-14 18:51:32 -0700

    new

---
+++ excercises/ex04.js
@@ -0,0 +1,19 @@
+let totalNumber = 0;
+
+//add a button titled click me
+
+$("#needy-button").click( function() {
+
+ totalNumber = totalNumber + 3;    
+
+let sentence = "EXP =";;
+ $("#needy-button").html( sentence + totalNumber );
+});
+//when it is clicked
+
+//show how many times
+//add one to the total number
+//show the total number
+//on our button
+//show "Clicked total number times"
+//a top limit
